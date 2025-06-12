76b7de1c6d65cb40107675f6a08e55e95c252f10
Eremina Dina <dindin1414@mail.ru>
2025-06-12 16:55:44 +0700

    ДЗ за 01.03.2025, сроком до 11.03.2025, задачи 10,11

---
+++ DZ/do25.03.11_v.01.03.25/do25.03.11_v.01.03.25/2025.03.01_1-10_TreugPascal.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define SPACE_BETWEEN	8
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int h;
+	cout << "Ввыедите высоту треугольника: "; cin >> h;
+	long long int nf = 1;
+	cout.width(SPACE_BETWEEN / 2 * (h + 1));
+	cout << "";
+	cout << "   " << 1 << "\n";
+	for (int n = 1; n <= h; n++)
+	{
+		nf *= n;	//n!
+		long long int mf = 1;
+		for (int i = 0; i < h - n; i++)cout << "    ";
+		cout.width(SPACE_BETWEEN);
+		//cout << left;
+		cout << 1 /*<< "       "*/;
+		for (int m = 1; m <= n; m++)
+		{
+			mf *= m;
+			long long int nmf = 1;
+			for (int nm = 1; nm <= n - m; nm++)
+			{
+				nmf *= nm;
+			}
+			cout.width(SPACE_BETWEEN);
+			cout << nf / mf / nmf /*<< "       "*/;
+		}
+		cout << endl;
+	}
+}
